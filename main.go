f47fd10ef0d9ad86b36b6469da6b2b079dc02a00
njprem <prem842547@gmail.com>
2026-01-01 22:49:37 +0700

    Add config function for gcloud , mostly check env that it empty or not. If empty assign it. If it still empty log.fatal(message)

--- main.go
+++ main.go
@@ -33,6 +33,35 @@ func mustInt (s string) int{
 	return i
 }
 
+func mustConfig () Config{
+	tz := getenv("TIMEZONE", "Asia/Bangkok")
+	dueDay := mustInt(getenv("DUE_DAY", "15"))
+
+	lineToken := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
+	if lineToken == ""{
+		log.Fatal("missing LINE_CHANNEL_ACCESS_TOKEN (inject from Secret Manager in Cloud Run)")
+	}
+
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == ""{
+		projectID = os.Getenv("GCP_PROJECT")
+	}
+	if projectID == ""{
+		log.Fatal("missing project id")
+	}
+
+	return Config{
+		Timezone: tz,
+		DueDay: dueDay,
+		GroupID: os.Getenv("GROUP_ID"),
+		AmountTHB: getenv("AMOUNT_THB", "150"),
+		MessageTemplate: getenv("MESSAGE_TEMPLATE", "อย่าลืมจ่ายค่า Apple One เดือนนี้ %s บาทนะครับ 🙏"),
+		LineAccessToken: lineToken,
+		LineChannelSecret: os.Getenv("LINE_CHANNEL_SECRET"),
+		FirestoreProjectID: projectID,		
+	}
+}
+
 func main() {
-	
+	cfg := mustConfig()
 }
